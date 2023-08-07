772841fdc1060a1357a6d34f0e47797f7faf6720
JieZhao03 <138514141+JieZhao03@users.noreply.github.com>
2023-08-07 23:59:51 +0800

    Create step0. list of R packages.R

---
+++ code/step0. list of R packages.R
@@ -0,0 +1,14 @@
+#======================
+# Install the packages
+#======================
+install.packages("stats", version = "4.0.2")
+install.packages("randomForest", version = "4.6-14")
+install.packages("mlr", version = "2.18.0")
+install.packages("DMwR", version = "0.4.1")
+install.packages("ggplot2", version = "3.3.6")
+install.packages("dplyr", version = "1.0.9")
+install.packages("ggrepel", version = "0.9.1")
+install.packages("ggsci", version = "2.9")
+install.packages("survminer", version = "0.4.8")
+install.packages("survival", version = "3.2-10")
+install.packages("stringr", version = "1.4.0")
